15e894df26efb52530052bc4ef8af8ef0d7c9950
lau-lhg <60954426+lau-lhg@users.noreply.github.com>
2021-02-11 19:14:45 -0600

    Update vaccination_strategy_SIR.r

--- vaccination_strategy_SIR.r
+++ vaccination_strategy_SIR.r
@@ -1,4 +1,4 @@
-#debugger
+#keep track of all movements
 mov<-c()
 
 #VACCINATION STRATEGY
@@ -7,17 +7,17 @@ strategy<-c('mobility','mortality')
 #INITIAL AMOUNT OF PEOPLE IN EACH SUBGROUP
 
 #age group 0-20
-S_1<- 8000
-I_1<- 0
+I_1<- 26
+S_1<- 44403 - I_1
 #age group 21-40
-S_2<- 8000
-I_2<- 1
+I_2<- 179
+S_2<- 40992 - I_2
 #age group 41-60
-S_3<- 8000
-I_3<- 0
+I_3<- 148
+S_3<- 29839 - I_3
 #age group 60+
-S_4<- 8000
-I_4<- 0
+I_4<- 56
+S_4<- 15029 - I_4
 
 #Put them in vectors for easier access
 S<-c(S_1, S_2, S_3, S_4)
@@ -26,17 +26,17 @@ I<-c(I_1, I_2, I_3, I_4)
 #total dead
 D<- 0
 #total recuperated
-R<- 0
+R<- 4018
 
 #average daily vaccination capacity (consider efficacy)
-V<- 100
+V<- 10
 
 #SUBGROUP parameters
 
 #death rate
 k<-c(0.01,0.02,0.09,0.13)
 #recuperation rate
-b<-(1-k)*(1/14)
+b<-(1-k)
 #mobility
 a<-c(2.25,3,2.75,2)
 
@@ -78,20 +78,21 @@ get_times<-function(pop,params){
   #iterate over each group
   for (i in 1:4){
     #infection
-    times[i]<-rexp(n=1,rate=params$a[i]*pop[(2*i-1)]*infected/N)
+    times[i]<-rexp(n=1,rate=(0.06)*params$a[i]*pop[(2*i-1)]*infected/N)
     #death
-    times[i+4]<-rexp(n=1,rate=params$k[i]*pop[(2*i)])
+    times[i+4]<-rexp(n=1,rate=params$k[i]*pop[(2*i)]*(1/17))
     #vaccination
-    times[i+12]<-rexp(n=1,rate=params$lambda[i])
+    times[i+12]<-rexp(n=1,rate=params$lambda[i]*pop[2*i-1]/pop[2*i-1])
     #recuperation
-    times[i+8]<-rexp(n=1,rate=params$b[i]*pop[(2*i)])
+    times[i+8]<-rexp(n=1,rate=params$b[i]*pop[(2*i)]*(1/14))
   }
   return(times)
 }
 
 get_lambda <- function(V, N, P){
   #distribute and normalize
-  lambda<-V*N*P/sum(N*P)
+  lambda<-c(0,0,0,0)
+  lambda [ which(P*N==max(P*N))] <- V
   return(lambda)
 }
 
@@ -109,47 +110,73 @@ set_lambda<-function(S, params, strategy, V){
   return(lambda)
 }
 
+#MAIN SIMULATION ITERATORS
+#We will fluctuate between strategies with different probability values (limit)
+#Each probability combination will be run e simulations
+#each simulation will run until herd immunity is achieved or no active cases remain
+#At the end of each simulation, a report will be printed
 
-#we are interested in two values
-out_time<-vector(mode='numeric')
-out_dead<-vector(mode='numeric')
 
-#number of iterations
-limit<-1
-#herd immunity at 75 percent
-herd_immunity<-sum(pop)*0.75
+#report titles
+print(c('mobility','pandemic_time','death_toll','vaccines','total_cases'))
+
+#number of iterations on strategy probability
+limit<-5
+#number of repetitions on with each strategy parameters
+reps<-10
+#amount of rec people needed for pandemic to end
+herd_immunity<-sum(pop[c(1:8,10)])*0.7
 
 #we will run the simulations with a range of probabilities
-for (j in 1:limit){
-  #time accumulator 
-  t<-0
+for (j in 0:limit){
   #new probability parameters for iteration
   strategyR<-c(j/limit,1-j/limit)
   
-  #run simulation
-  while( pop[10]<=herd_immunity & sum(pop[c(2,4,6,8)])>=0 ){
-    #select strategy according to probabilities
-    strat<-sample(c('mobility','mortality'), size=1, prob=strategyR)
-    #update lambda values according to strategy
-    params$lambda<-set_lambda(S=pop[c(1,3,5,7)],params=params,strategy=strat, V=V)
-    #obtain times
-    times<-get_times(pop,params)
-    #identify next event
-    chosen<-which(times==min(times[which(times>0)]))[1]
-    #execute next event
-    pop<-pop+(poss[,chosen])
-    #accumulate time
-    t<-t+times[chosen]
+  #repetitions per value
+  for (e in 1:reps){
     
-    #DEBUGGER
-    mov<-append(mov,chosen)
-  }
-  #save time and number of deaths for iteration
-  out_time<-append(out_time,t,after=j)
-  out_dead<-append(out_dead,pop[9],after=j)
-  #reset population values for new iterations
-  pop<-as.vector(c(rbind(S,I),D,R))
-}
+    #time accumulator 
+    t<-0
+    
+    #run simulation
+    while( sum(pop[c(2,4,6,8,10)]) < herd_immunity ){
+      
+      #select strategy according to probabilities
+      strat<-sample(strategy, size=1, prob=strategyR)
+      #update lambda values according to strategy
+      params$lambda<-set_lambda(S=pop[c(1,3,5,7)],params=params,strategy=strat, V=V)
+      #obtain times
+      times<-get_times(pop,params)
+      #identify next event
+      chosen<-which(times==min(times[which(times>0)]))[1]
+      #execute next event
+      pop<-pop+(poss[,chosen])
+      #accumulate time
+      t<-t+times[chosen]
+      #update amount of susceptible people needed for pandemic to end
+      herd_immunity<-sum(pop[c(1:8,10)])*0.7
+      #register event
+      mov<-append(mov,chosen)
+      
+    }#end of simulation
+    
+    #save important output info for analysis
+    tot_vacc<-sum(mov>=13)
+    tot_cases<-sum(mov<=4)
+    output<-c(strategyR[1],t,pop[[9]],tot_vacc,tot_cases)
+    
+    #print and save output
+    print(output)
+    
+    #reset population values for new simulation repetitions
+    pop<-as.vector(c(rbind(S,I),D,R))
+    mov<-c()
+    
+  }#end of value repetitions
+  
+}#end of whole enchilada
+
+print('DONE')
 
 
 
